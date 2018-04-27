098bca8d5f40c1eacf7825787769b265896e05e1
monika-pant <monikapant.1feb@gmail.com>
2018-04-27 16:47:02 +0530

    peer to peer messaging

---
+++ index.js
@@ -0,0 +1,21 @@
+var Peer=require('simple-peer');
+var peer=new Peer({
+    initiator: location.hash === '#init',
+    trickle: false
+    // stream: streams
+});
+
+peer.on('signal',function(data){
+    document.getElementById('yourId').value=JSON.stringify(data);
+});
+document.getElementById('connect').addEventListener('click',function(){
+    var otherId=JSON.parse(document.getElementById('otherId').value);
+    peer.signal(otherId);
+});
+document.getElementById('send').addEventListener('click',function(){
+    var yourMessage=document.getElementById('yourmessage').value;
+    peer.send(yourMessage);
+});
+peer.on('data',function(data){
+    document.getElementById('messages').textContent += data + '\n';
+});
